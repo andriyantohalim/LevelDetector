588399d53fa4a79ee17c89649305b137f316056f
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-08-31 22:15:57 +0800

    Initial Commit

---
+++ LevelDetector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LevelDetector
+//
+//  Created by Andriyanto Halim on 31/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LevelDetector/LevelDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelDetectorApp.swift
+//  LevelDetector
+//
+//  Created by Andriyanto Halim on 31/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LevelDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
